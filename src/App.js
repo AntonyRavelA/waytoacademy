f871714f84de1925249c6e30198f43f9c468e910
Srinithi89 <2018it0609@svce.ac.in>
2021-06-01 23:11:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Route
 } from "react-router-dom";
 import Header from './Ui/Header/Header'
-import Home  from './components/Home/Home'
+import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import CreateClass from './components/CreateClass/CreateClass'
@@ -15,8 +15,8 @@ function App() {
   return (
     <Router>
       <div className="container-fluid">
-        <Header />        
-        <Switch>        
+        {/* <Header />         */}
+        <Switch>
           <Route path="/login">
             <Login />
           </Route>
