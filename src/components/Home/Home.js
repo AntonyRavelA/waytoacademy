2ccbadc6bf10a618da89238214d41c6d76bbcd30
Srinithi89 <2018it0609@svce.ac.in>
2021-06-01 23:25:03 +0530

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,32 @@
+import React from 'react';
+// import Title from './Title';
+// import Profile from './Profile';
+import Nav from './Nav';
+import Container from './container';
+import './App.css';
+import logo from './images/logo.png';
+import Button from './button';
+const Home = () => {
+    return (
+        <div>
 
-const Home = () => <h2>Home</h2>;
+
+
+
+            <Nav />
+
+
+
+            <Button />
+
+
+
+            <Container />
+
+
+
+        </div>
+    );
+};
 
 export default Home;
