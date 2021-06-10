45c6cb5fb7355522dbe13204055179f8afb5ad99
Antony Ravel A <antonyravel@gmail.com>
2021-06-10 13:12:29 +0530

    commit (#11)
    Co-authored-by: M-Varshini <varshinidmd@gmail.com>
    Co-authored-by: AntonyRavel Arulppan <aarulappan@ashleyfurniture.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-import Header from './Ui/Header/Header'
+import Header from "./headerlogo";
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
@@ -12,40 +13,41 @@ import CreateClass from './components/CreateClass/CreateClass'
 import MyProfile from './components/MyProfile/MyProfile'
 import Classes from './components/Classes/Classes'
 import Display from './components/Classes/Display'
-
-function App() {
-  return (
-    <Router>
-      <div className="container-fluid">
-
-        <Header />
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/create-class">
-            <CreateClass />
-          </Route>
-          <Route path="/my-profile">
-            <MyProfile />
-          </Route>
-          <Route path="/product/:producttype?">
-            <Classes />
-          </Route>
-          <Route path="/product/:producttype?/:id?">
-            <Display />
-          </Route>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          
-        </Switch>
-      </div>
-    </Router>
-  );        
+class App extends React.Component{
+    render() {
+        return(
+          <Router>
+          <div className="container-fluid">
+    
+            <Header />
+            <Switch>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+              <Route path="/create-class">
+                <CreateClass />
+              </Route>
+              <Route path="/my-profile">
+                <MyProfile />
+              </Route>
+              <Route path="/product/:producttype?">
+                <Classes />
+              </Route>
+              <Route path="/product/:producttype?/:id?">
+                <Display />
+              </Route>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              
+            </Switch>
+          </div>
+        </Router>
+        )
+        
     }
 
 }
