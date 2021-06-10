056314fbaf4c7af7742dfdbd35f490ca60885261
Antony Ravel A <antonyravel@gmail.com>
2021-06-10 12:38:18 +0530

    Praveen t (#8)
    * class change
    
    * commit
    
    * Regiter Page commit
    
    * commit
    
    Co-authored-by: Praveen-pranav <52925176+Praveen-pranav@users.noreply.github.com>
    Co-authored-by: AntonyRavel Arulppan <aarulappan@ashleyfurniture.com>

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import Nav from './Nav';
 import Container from './container';
 import './App.css';
-import logo from './images/logo.png';
 import Button from './button';
 const Home = () => {
     return (
